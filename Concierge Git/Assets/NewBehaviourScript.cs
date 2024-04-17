8d70320a215335842fad2937301fb14f1e1a5996
ehsu092 <ehsu031@ucr.edu>
2024-04-17 01:55:17 -0700

    Initial Test Commit
    3 test objects in different colors

---
+++ Concierge Git/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GDT_ChangeMaterial : MonoBehaviour
+{
+    Material myMaterial
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
