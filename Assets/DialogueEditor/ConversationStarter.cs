ed046cfd047c24cf34b6cf85182ecef3d81ad958
carinetham <carinechiawen@gmail.com>
2024-04-29 20:37:38 -0700

    dialogue box
    dialogue box shows up but the mouse is funky and cant click properly

--- Assets/DialogueEditor/ConversationStarter.cs
+++ Assets/DialogueEditor/ConversationStarter.cs
@@ -7,14 +7,32 @@ public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
 
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            //if (Cursor.lockState != CursorLockMode.Confined)
+            //{
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            //}
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Update();
             if (Input.GetKeyDown(KeyCode.F))
             {
                 ConversationManager.Instance.StartConversation(myConversation);
             }
         }
     }
+
 }
