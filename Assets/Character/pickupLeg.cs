14db1e963c55a8d9ac9349cfc32ab7ffd4404b6b
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2024-05-22 06:37:45 -0700

    update pick up successfully

--- Assets/Character/pickupLeg.cs
+++ Assets/Character/pickupLeg.cs
@@ -1,140 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;  // Add this to use UI components
-
-// public class PickupLeg : MonoBehaviour
-// {
-//     public GameObject interactionText;  // Reference to the UI Text element
-//     // public GameObject legNotPickup;
-//     // public GameObject legPickup;
-//     public AudioSource pickupSound;
-//     public float interactionDistance = 2.0f;  // Distance within which the Character can interact
-
-//     private bool isInteractable;
-//     // private GameObject Character;
-//     public object switch_scence;
-//     void Start(){
-//         switch_scence.SetActive(false);
-//     }
-
-//     private void OnTriggerStay(collider other){
-//         if(other.gameObject.tag == "MainCamera"){
-//             if(Input.GetKeyDown(KeyCode.E)){
-//                 this.gameObject.SetActive(false);
-//                 switch_scence.SetActive(true);
-//             }
-//         }
-//     }
-    // void Start()
-    // {
-    //     // Find the Character object by accessing the first person controller
-    //     GameObject firstPersonController = GameObject.Find("Character");
-    //     if (firstPersonController != null)
-    //     {
-    //         Character = firstPersonController.transform.GetChild(0).gameObject;
-    //         if (Character == null)
-    //         {
-    //             Debug.LogError("Character not found. Make sure the Character object is a child of the FPSController.");
-    //         }
-    //     }
-    //     else
-    //     {
-    //         Debug.LogError("FPSController not found. Make sure the FPSController object is in the scene.");
-    //     }
-
-    //     // Hide the interaction text initially
-    //     if (interactionText != null)
-    //     {
-    //         interactionText.SetActive(false);
-    //     }
-    // }
-
-    // void Update()
-    // {
-    //     if (isInteractable && Input.GetKeyDown(KeyCode.E))
-    //     {
-    //         interactionText.SetActive(false);
-    //         isInteractable = false;
-
-    //         // Play pickup sound if assigned
-    //         if (pickupSound != null)
-    //         {
-    //             pickupSound.Play();
-    //         }
-
-    //         // Toggle leg game objects
-    //         if (legPickup != null)
-    //         {
-    //             legPickup.SetActive(true);
-    //         }
-
-    //         if (legNotPickup != null)
-    //         {
-    //             legNotPickup.SetActive(false);
-    //         }
-    //     }
-
-    //     // Check the interaction distance
-    //     if (IsCharacterClose())
-    //     {
-    //         if (interactionText != null)
-    //         {
-    //             interactionText.SetActive(true);
-    //             isInteractable = true;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (interactionText != null)
-    //         {
-    //             interactionText.SetActive(false);
-    //             isInteractable = false;
-    //         }
-    //     }
-    // }
-
-    // private bool IsCharacterClose()
-    // {
-    //     if (Character != null)
-    //     {
-    //         // Calculate the distance between the Character and the object
-    //         float distance = Vector3.Distance(Character.transform.position, transform.position);
-
-    //         // Debug distance for troubleshooting
-    //         Debug.Log("Distance to object: " + distance);
-
-    //         // Return true if the distance is within the interaction distance threshold
-    //         return distance <= interactionDistance;
-    //     }
-    //     else
-    //     {
-    //         return false;
-    //     }
-    // }
-
-    // void OnTriggerStay(Collider other)
-    // {
-    //     // Ensure the Character is within interaction distance
-    //     if (other.CompareTag("MainCamera") && IsCharacterClose())
-    //     {
-    //         Debug.Log("Character is within the trigger area.");
-    //         interactionText.SetActive(true);
-    //         isInteractable = true;
-    //     }
-    // }
-
-    // void OnTriggerExit(Collider other)
-    // {
-    //     if (other.CompareTag("MainCamera"))
-    //     {
-    //         Debug.Log("Character has exited the trigger area.");
-    //         interactionText.SetActive(false);
-    //         isInteractable = false;
-    //     }
-    // }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -142,37 +5,107 @@ using UnityEngine.UI;  // Add this to use UI components
 
 public class PickupLeg : MonoBehaviour
 {
-    public GameObject interactionText;
-    public AudioSource pickupSound;
-    public GameObject switchScene;
-    public float interactionDistance = 2.0f;
+    public GameObject interactionText;  // Reference to the UI Text element
+    public GameObject legNotPickup;     // Reference to the leg object before pickup
+    public GameObject legPickup;        // Reference to the leg object after pickup
+    public AudioSource pickupSound;     // Reference to the pickup sound
+    public float interactionDistance = 2.0f;  // Distance within which the Character can interact
 
     private bool isInteractable;
+    private GameObject character;
 
     void Start()
     {
-        if (switchScene != null)
+        // Find the Character object by accessing the first person controller
+        GameObject firstPersonController = GameObject.Find("Character");
+        if (firstPersonController != null)
         {
-            switchScene.SetActive(false);
+            character = firstPersonController.transform.GetChild(0).gameObject;
+            if (character == null)
+            {
+                Debug.LogError("Character not found. Make sure the Character object is a child of the FPSController.");
+            }
+        }
+        else
+        {
+            Debug.LogError("FPSController not found. Make sure the FPSController object is in the scene.");
+        }
+
+        // Hide the interaction text initially
+        if (interactionText != null)
+        {
+            interactionText.SetActive(false);
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    void Update()
     {
-        if (other.gameObject.CompareTag("Player")) // Assuming player has "Player" tag
+        if (character == null)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            return;
+        }
+
+        // Check the interaction distance
+        if (IsCharacterClose())
+        {
+            if (interactionText != null)
             {
-                gameObject.SetActive(false); // Disable the leg object
+                interactionText.SetActive(true);
+                isInteractable = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E) && isInteractable)
+            {
+                interactionText.SetActive(false);
+                isInteractable = false;
+
+                // Play pickup sound if assigned
                 if (pickupSound != null)
                 {
                     pickupSound.Play();
                 }
-                if (switchScene != null)
+
+                // Toggle leg game objects
+                if (legPickup != null)
                 {
-                    switchScene.SetActive(true); // Activate the object responsible for scene switching
+                    legPickup.SetActive(true);
                 }
+
+                if (legNotPickup != null)
+                {
+                    legNotPickup.SetActive(false);
+                }
+
+                // Deactivate this object
+                gameObject.SetActive(false);
             }
+        }
+        else
+        {
+            if (interactionText != null)
+            {
+                interactionText.SetActive(false);
+                isInteractable = false;
+            }
+        }
+    }
+
+    private bool IsCharacterClose()
+    {
+        if (character != null)
+        {
+            // Calculate the distance between the character and the object
+            float distance = Vector3.Distance(character.transform.position, transform.position);
+
+            // Debug distance for troubleshooting
+            Debug.Log("Distance to object: " + distance);
+
+            // Return true if the distance is within the interaction distance threshold
+            return distance <= interactionDistance;
+        }
+        else
+        {
+            return false;
         }
     }
 }
