dca6e370ab0ba2cea626179b23a2ae6d2b50260b
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2024-05-29 12:08:53 -0700

    update

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -1,36 +1,81 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Teleporter : MonoBehaviour
+// {
+//     public GameObject Player;
+//     public GameObject TeleportTo;
+//     public GameObject StartTeleporter;
+
+//     private void OnTriggerEnter(Collider collision)
+//     {
+//         Debug.Log("OnTriggerEnter called with: " + collision.gameObject.name);
+
+//         if (collision.gameObject.CompareTag("Teleporter"))
+//         {
+//             Debug.Log("Teleporter triggered");
+//             Player.transform.position = TeleportTo.transform.position;
+//         }
+
+//         if (collision.gameObject.CompareTag("SecondTeleporter"))
+//         {
+//             Debug.Log("SecondTeleporter triggered");
+//             Player.transform.position = StartTeleporter.transform.position;
+//         }
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
- 
+
 public class Teleporter : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject TeleportTo;
-    public GameObject StartTeleporter;
-    private GameObject bottoms;
- 
-    private void OnTriggerEnter(Collider collision)
+    public int code;
+    float disableTimer = 0;
+
+    void Update()
     {
-        bottoms = GameObject.Find("check1");
-
-        if (bottoms == null)
+        if (disableTimer > 0)
         {
-            Debug.LogError("Bottoms object not found in the scene.");
+            disableTimer -= Time.deltaTime;
+            Debug.Log("Timer running: " + disableTimer);
         }
+    }
 
-        if (collision.gameObject.CompareTag("Teleporter"))
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("OnTriggerEnter called with: " + collider.gameObject.name);
+
+        if (collider.CompareTag("Player") && disableTimer == 0)
         {
-            Player.transform.position = TeleportTo.transform.position;
-            Player.transform.rotation = TeleportTo.transform.rotation;
-            // Player.transform.SetParent(TeleportTo);
+            Debug.Log("Player entered teleporter with code: " + code);
+
+            Teleporter[] teleporters = FindObjectsOfType<Teleporter>();
+            Debug.Log("Found teleporters: " + teleporters.Length);
+
+            foreach (Teleporter tp in teleporters)
+            {
+                Debug.Log("Checking teleporter with code: " + tp.code);
+
+                if (tp.code == code && tp != this)
+                {
+                    Debug.Log("Teleporting to: " + tp.gameObject.name);
+
+                    tp.disableTimer = 2;
+                    Vector3 position = tp.gameObject.transform.position;
+                    position.y += 2;
+                    collider.gameObject.transform.position = position;
+
+                    Debug.Log("Teleported player to: " + position);
+                }
+            }
         }
- 
-        // if (collision.gameObject.CompareTag("SecondTeleporter"))
-        // {
-        //     Player.transform.position = StartTeleporter.transform.position;
-        // }
+        else
+        {
+            Debug.Log("Conditions not met for teleportation.");
+        }
     }
 }
