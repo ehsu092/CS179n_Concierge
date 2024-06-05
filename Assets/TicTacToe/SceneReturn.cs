8201fa53455a33d4d23cac18ba768171473a14e8
adamtruth <adamamin54@gmail.com>
2024-06-05 15:07:30 -0700

    Update SceneReturn.cs

--- Assets/TicTacToe/SceneReturn.cs
+++ Assets/TicTacToe/SceneReturn.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(LineRenderer))]
 public class SceneReturn : MonoBehaviour
 {
+
+    private bool challengePassed = false;
+
     void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(0);
+        IncrementChallenge();
+        challengePassed = true;
+    }
+    private void IncrementChallenge()
+    {
+        // Find the pickup_flashlight script and call IncrementChallenge method
+        pickup_flashlight pickupScript = FindObjectOfType<pickup_flashlight>();
+        if (pickupScript != null)
+        {
+            pickupScript.IncrementChallenge();
+        }
+        else
+        {
+            Debug.LogError("pickup_flashlight script not found.");
+        }
     }
 }
 
